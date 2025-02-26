31826119f1b6e63a5c2904c43e40da3259a98fd3
RESEGO MOTLHASI <contactresego@gmail.com>
2025-02-26 13:19:36 +0200

    implemented a simple code for the library management system

---
+++ library.py
@@ -0,0 +1,34 @@
+class Book:
+    def __init__(self, title, author, ISBN, available_copies):
+        self. title = title
+        self.author = author
+        self.ISBN = ISBN
+        self.available_copies = available_copies
+
+    def borrow_book(self, title):
+        self.tile = title
+        self.available_copies -= 1
+        if title:
+            print(f"You borrowed a book {title}, by {self.author}, ISBN: {self.ISBN} ")
+        else:
+            print("{title} not found on the shelf")
+        pass
+
+    def return_book(self, title):
+        self.title = title
+        self.available_copies += 1
+        print(f"You returned the book {title}, by {self.author}, ISBN: {self.ISBN}. ")
+        pass
+
+    def check_availability(self, title):
+        self.title = title
+        if self.available_copies > 0:
+            print(f"{self.available_copies} copies of {title} is available")
+        else:
+            print(f"No copies of {title} is available")
+        pass
+
+library = Book("Nothing but the truth", "John Kani", "223-445-565-67", 45)
+library.borrow_book("Nothing but the truth")
+library.return_book("Nothing but the truth")
+library.check_availability("Nothing but the truth")
